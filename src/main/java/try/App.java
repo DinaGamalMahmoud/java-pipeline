d8f5f0eb771de5dfe1b974dff14f68c3fd0f65a0
Dina Gamal Mahmoud labib <46623409+DinaGamalMahmoud@users.noreply.github.com>
2019-06-08 11:50:08 +0200

    Add files via upload

---
+++ src/main/java/try/App.java
@@ -0,0 +1,20 @@
+package try;
+
+//Java Pipeline!
+
+public class App
+{
+
+    private final String message = "Java Pipeline!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
