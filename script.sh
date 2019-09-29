c411f72d219f41e1fbf4c21ba849a5813a466ded
dinagamal1 <dina.gamal1@vodafone.com>
2019-09-29 14:01:24 +0200

    add readme

--- script.sh
+++ script.sh
@@ -9,30 +9,30 @@ GITHUB_TOKEN="df4d8c981650cfe8dc9bd68ebd86760295d8204f"
 git config --local user.email $GIT_EMAIL
 git config --local user.name  $GIT_USER
 
+git remote add upstream https://github.com/DinaGamalMahmoud/java-pipeline
+
+git remote add origin https://github.com/dinagamal1/java-pipeline
+
 curl -u $GIT_USER https://$GITHUB_TOKEN@api.github.com/repos/$upstream_repo_username/testrepo/forks -d ''
 
 curl -u $GIT_USER https://$GITHUB_TOKEN@api.github.com/repos/$upstream_repo_username/java-pipeline/forks -d ''
 
-git clone https://github.com/$GIT_USER/java-pipeline
+git pull https://github.com/$GIT_USER/java-pipeline master
 
-git clone https://github.com/$GIT_USER/testrepo trysubmodules/
 
+
+git pull https://github.com/dinagamal1/testrepo master trysubmodules
 
 git submodule update --init --remote -- trysubmodules/testrepo
 
 git add .
-git commit -m "update submodule"
+git commit -m "add readme"
 
 
 
 git push https://github.com/dinagamal1/java-pipeline master
 
 git remote add upstream https://github.com/DinaGamalMahmoud/java-pipeline
+git remote -v
 
-git remote add origin https://github.com/dinagamal1/java-pipeline
-
-
-git request-pull origin/master upstream/master
-
-
-
+hub pull-request --base DinaGamalMahmoud:master --head dinagamal1:master
